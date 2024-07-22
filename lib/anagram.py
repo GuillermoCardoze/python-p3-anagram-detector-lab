54466dad8053e51c6761694ae81e047397103b1c
Guillermo <vital_tmv@yahoo.com>
2024-07-22 18:01:03 -0500

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+import ipdb
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, words):
+        return [w for w in words if sorted(w) == self.sorted_word]
+
